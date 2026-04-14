f1a812667d2cc9d1c67b262235678869b8c76420
Simon Scholz <Simon.Sleeeep@outlook.com>
2026-04-14 17:36:55 +0800

    chore: Update monochrome icon

---
+++ app/src/main/java/com/mocharealm/compound/domain/usecase/GetChatPinnedMessageUseCase.kt
@@ -0,0 +1,12 @@
+package com.mocharealm.compound.domain.usecase
+
+import com.mocharealm.compound.domain.model.Message
+import com.mocharealm.compound.domain.repository.TelegramRepository
+
+class GetChatPinnedMessageUseCase(
+    private val telegramRepository: TelegramRepository
+) {
+    suspend operator fun invoke(chatId: Long): Result<Message?> {
+        return telegramRepository.getChatPinnedMessage(chatId)
+    }
+}
